897fb311d1595e3231376a1472f79eb35af93196
Michael O'Keefe <okeefm@rpi.edu>
2012-06-27 09:44:19 -0400

    Updated GetCurrentLocation to make it update the current location

--- src/edu/rpi/communitysensors/android/GetCurrentLocation.java
+++ src/edu/rpi/communitysensors/android/GetCurrentLocation.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,23 +17,125 @@ public class GetCurrentLocation {
 	private Context mContext;
 	private static final long minDistance = 1; //in meters
 	private static final long minTime = 10000; //in milliseconds
+	private static final int TWO_MINUTES = 1000 * 60 * 2;
+	public Location currentLocation;
+	private Boolean usingLastKnownLocation;
 	
 	public GetCurrentLocation(Context c) {
 		mContext = c;
+		locationManager=(LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+		listener = new LocationListener() {
+        	public void onLocationChanged(Location location) {
+        		Toast.makeText(mContext, "location updated", Toast.LENGTH_SHORT);
+        		updateCurrentLocation(location);
+        	}
+        	
+        	public void onStatusChanged(String provider, int status, Bundle extras) {
+        		String message;
+        		switch (status) {
+        			case LocationProvider.AVAILABLE: message = "Location now available"; break;
+        			case LocationProvider.TEMPORARILY_UNAVAILABLE: message = "Location temporarily unavailable"; break;
+        			case LocationProvider.OUT_OF_SERVICE: message = "Location unavailable"; break;
+        			default: message = "Location status changed"; break;
+        		}
+        		
+        		Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+        	}
+        	
+        	public void onProviderEnabled(String provider) {
+        		Toast.makeText(mContext, "Location provider enabled:" + provider, Toast.LENGTH_SHORT);
+        	}
+        	
+        	public void onProviderDisabled(String provider) {
+        		Toast.makeText(mContext, "Please enable GPS for location to work", Toast.LENGTH_LONG).show();
+        	}
+        };
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, listener);
+		currentLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		locationManager.removeUpdates(listener);
+        usingLastKnownLocation = true;
+        
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, listener);
 	}
+	
+	/** Determines whether one Location reading is better than the current Location fix
+	  * @param location  The new Location that you want to evaluate
+	  * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	  */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+	    if (usingLastKnownLocation) {
+	        // A new location is always better than the "last known location"
+	    	usingLastKnownLocation = false;
+	        return true;
+	    }
+
+	    // Check whether the new location fix is newer or older
+	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+	    boolean isNewer = timeDelta > 0;
+
+	    // If it's been more than two minutes since the current location, use the new location
+	    // because the user has likely moved
+	    if (isSignificantlyNewer) {
+	        return true;
+	    // If the new location is more than two minutes older, it must be worse
+	    } else if (isSignificantlyOlder) {
+	        return false;
+	    }
+
+	    // Check whether the new location fix is more or less accurate
+	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+	    boolean isLessAccurate = accuracyDelta > 0;
+	    boolean isMoreAccurate = accuracyDelta < 0;
+	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+	    // Check if the old and new location are from the same provider
+	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+	            currentBestLocation.getProvider());
+
+	    // Determine location quality using a combination of timeliness and accuracy
+	    if (isMoreAccurate) {
+	        return true;
+	    } else if (isNewer && !isLessAccurate) {
+	        return true;
+	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+	    if (provider1 == null) {
+	      return provider2 == null;
+	    }
+	    return provider1.equals(provider2);
+	}
+	
+	/** updates the current location, based on if the new location is better */
+    public void updateCurrentLocation(Location location) {
+    	if (isBetterLocation(location, currentLocation)) {
+    		currentLocation = location;
+    		usingLastKnownLocation = false;
+    	}
+    }
 	
 	//This function gets the location and returns a location value
 	//@return - current location value in location format
-	public Location getLocation(){		
-		locationManager=(LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-		listener = new MyLocationListener();
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, listener);
-		Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		return location;
+	public Location getLocation() {
+		return currentLocation;
+	}
+
+	
+	public void stopLocationUpdate() {
+		locationManager.removeUpdates(listener);
 	}
 	
-	public void stopLocationUpdate(){
-		locationManager.removeUpdates(listener);
+	public void resumeLocationUpdate() {
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, listener);
+    	currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    	usingLastKnownLocation = true;
 	}
 	
 	//This class takes in a location and returns latitude as a double
@@ -161,31 +264,4 @@ public class GetCurrentLocation {
 		return LatLong;
 	}
 	
-	//This class is meant to handle the LocationListener
-	//It was implemented based off of code from:
-	//http://www.javacodegeeks.com/2010/09/android-location-based-services.html
-	//http://www.firstdroid.com/2010/04/29/android-development-using-gps-to-get-current-location-2/
-	//http://catchmayuri.blogspot.com/2011/05/get-current-location-gps-position-in.html
-	public class MyLocationListener implements LocationListener {	
-		@Override
-		public void onLocationChanged(Location loc) {
-			//String message = String.format("New Location \n Longitude: %1$s \n Latitude: %2$s", loc.getLongitude(), loc.getLatitude());
-			//Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();		
-		}
-	
-		@Override
-		public void onProviderDisabled(String provider) {
-			//Toast.makeText(mContext, "GPS is disabled by user.", Toast.LENGTH_SHORT).show();
-		}
-	
-		@Override
-		public void onProviderEnabled(String provider) {
-			//Toast.makeText(mContext, "GPS is enabled by user.", Toast.LENGTH_SHORT).show();
-		}
-	
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
-			//Toast.makeText(mContext, "Provider status has changed.", Toast.LENGTH_SHORT).show();
-		}	
-	}
 }
